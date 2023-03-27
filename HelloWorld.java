572efd0d56ea846e6eacedbebcbd52933f3f05e6
Saptarshi  Paul <ohm@Saptarshis-MacBook-Pro.local>
2023-03-27 15:16:33 -0400

    added a small change

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("______******** Hello World and Welcome to Jenkins Training *********"
+		System.out.println("______******** Hello World and Welcome to Jenkins Training! *********"
 				+ "________");
 
 	}
