3e068e1b606d4dc0c3704db11aa292e3f5aaead5
Saptarshi  Paul <ohm@Saptarshis-MacBook-Pro.local>
2023-03-27 15:08:25 -0400

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("______******** Hello World and Welcome to Jenkins Training *********"
+				+ "________");
+
+	}
+
+}
